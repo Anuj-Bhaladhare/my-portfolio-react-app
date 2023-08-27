49f7143123e361901e49e9a73580ded02d7660d7
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-08-27 08:54:54 +0530

    update navbaar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import React from "react";
+import AnujPhoto from "../assest/anuj photo.jpg"
 
 const Home = () => {
     return(
         <div>
-            This is home page
+            <div className="flex">
+               <div>
+                  hello jeeeeeeeeeeeeeeee
+               </div>
+               <div>
+                  <img src={AnujPhoto} />
+               </div>
+            </div>
+
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
         </div>
     )
 }
--- src/components/Navbaar.js
+++ src/components/Navbaar.js
@@ -1,34 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbaar = () => {
+const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center p-4 bg-gray-900 text-white">
-            <div>
-                <Link to="/">
-                    <h2 className="text-xl font-bold">LOGO</h2>
-                </Link>
+        <nav className="flex justify-between items-center p-4 bg-gray-900">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-white font-bold text-xl">Logo</div>
+                <div className="hidden md:flex space-x-4">
+                    <Link to="/" className="text-white hover:text-blue-300">Home</Link>
+                    <Link to="/project" className="text-white hover:text-blue-300">Projects</Link>
+                    <Link to="/about" className="text-white hover:text-blue-300">About</Link>
+                    <Link to="/page" className="text-white hover:text-blue-300">Page</Link>
+                    <Link to="/contact" className="text-white hover:text-blue-300">Contact</Link>
+                </div>
+                <div className="md:hidden">
+                    <button className="text-white">
+                        <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </button>
+                </div>
             </div>
-
-            <ul className="flex space-x-6">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/project">Projects</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/page">Page</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
         </nav>
     );
 }
 
-export default Navbaar;
+export default Navbar;
