f13afb62f0a8031c4353dace2d6947dc98634740
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-09-04 19:46:49 +0530

    updaate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from "react";
 import "./App.css";
-import { Router, Routes, Route } from "react-router-dom";
-import About from "./components/core/About";
-import Contact from "./components/core/Contact";
-import Home from "./components/pages/Home";
-import Project from "./components/core/Projects";
+import About from "./components/pages/About";
+import Projects from "./components/pages/Projects";
 import Footer from "./components/comman/Footer";
 import Navbar from "./components/comman/Navbaar";
+import Introduction from "./components/pages/Introduction";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <Router>
       <div>
         <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-
+        <Introduction />
+        <About />
+        <Projects />
+        <Contact />
         <Footer />
       </div>
-    </Router>
   );
 }
 
